30c718412b3f6d063450aa4deadb5e087c0e361d
Felix Wittwer <dev@felixwittwer.de>
2017-11-09 23:39:58 +0100

    Implement slow sort, close #2

--- src/slowsort.rs
+++ src/slowsort.rs
@@ -3,8 +3,27 @@ pub trait Slowsort {
 }
 
 
-impl<T: Ord> Slowsort for Vec<T> {
+fn slowsort_step<T: PartialOrd + Copy>(vec: &mut Vec<T>, i: usize, j: usize) {
+    if i < j {
+        let m: usize = (i + j) / 2;
+        slowsort_step(vec, i, m);
+        slowsort_step(vec, m + 1, j);
+
+        if vec[j] < vec[m] {
+            // switch vec[j] and vec[m]
+            let buffer = vec[m];
+            vec[m] = vec[j];
+            vec[j] = buffer;
+        }
+
+        slowsort_step(vec, i, j - 1);
+    }
+}
+
+
+impl<T: PartialOrd + Copy> Slowsort for Vec<T> {
     fn slowsort(&mut self) {
-        unimplemented!();
+        let length = self.len();
+        slowsort_step(self, 0, length - 1);
     }
 }
